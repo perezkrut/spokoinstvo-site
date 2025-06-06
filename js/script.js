e9cec6b7ba24d4d80293d850833a053ec52530c0
perezkrut <swaworkatel@gmail.com>
2025-06-06 19:35:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
-let count = 0;
-function increment() {
-  count++;
-  document.getElementById('counter').textContent = count;
-}
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('orderForm');
-  if (form) {
-    form.addEventListener('submit', function(e) {
-      e.preventDefault();
-      alert('Заявка отправлена!');
-      form.reset();
-    });
-  }
-});
+
